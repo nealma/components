ebba53b1a889a8f8a7cbd1f34897d57a4481ea03
neal.ma <neal.ma.sh@gmail.com>
2016-05-19 22:52:09 +0800

    update pom extends

--- components-rest/src/main/java/com/nealma/account/rest/AccountController.java
+++ components-rest/src/main/java/com/nealma/account/rest/AccountController.java
@@ -1,7 +1,6 @@
 package com.nealma.account.rest;
 
-//import com.nealma.framework.domain.MessageBean;
-import org.springframework.http.converter.protobuf.ProtobufHttpMessageConverter;
+import com.nealma.framework.domain.MessageBean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,12 +12,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping
 public class AccountController {
 
-//    @RequestMapping("/accounts/login")
-//    public MessageBean login(@RequestParam(defaultValue = "", value = "username", required = true) String username,
-//                         @RequestParam(defaultValue = "", value = "password", required = true) String password) {
-//        MessageBean messageBean = new MessageBean();
-//        messageBean.setStatus(200);
-//        messageBean.setMessage("success");
-//        return messageBean;
-//    }
+    @RequestMapping("/accounts/login")
+    public MessageBean login(@RequestParam(defaultValue = "", value = "username", required = true) String username,
+                         @RequestParam(defaultValue = "", value = "password", required = true) String password) {
+        MessageBean messageBean = new MessageBean();
+        messageBean.setStatus(200);
+        messageBean.setMessage("success");
+        return messageBean;
+    }
 }
